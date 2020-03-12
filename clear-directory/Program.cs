f402b8584c0f818a5dbefbe3e308db4a2f1d4001
shin3250 <git@shin3250.net>
2020-03-12 13:06:53 +0900

    First commit

---
+++ clear-directory/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace clear_directory {
+    class Program {
+        static int Main(string[] args) {
+            if(args.Length != 1) {
+                Console.WriteLine("対象ディレクトリを指定してください。");
+                return -1;
+            }
+
+            if(!Directory.Exists(args[0])) {
+                Console.WriteLine("対象ディレクトリが存在しません。");
+                return -1;
+            }
+
+            foreach(var file in Directory.GetFiles(args[0], "*.*", SearchOption.TopDirectoryOnly)) {
+                try { File.Delete(file); } catch { };
+            }
+
+            foreach(var dir in Directory.GetDirectories(args[0], "*.*", SearchOption.TopDirectoryOnly)) {
+                try { Directory.Delete(dir, true); } catch { };
+            }
+
+            return 0;
+        }
+    }
+}
