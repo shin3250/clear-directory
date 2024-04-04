dfcc2535c6a30ca56f60adc7ed5177769c19eba8
shin3250 <git@shin3250.net>
2024-04-04 16:09:57 +0900

    読み取り専用ファイルも強制的に削除するよう変更

--- clear-directory/Program.cs
+++ clear-directory/Program.cs
@@ -4,22 +4,44 @@ using System.IO;
 namespace clear_directory {
     class Program {
         static int Main(string[] args) {
-            if(args.Length != 1) {
+#if DEBUG
+            args = new string[] { @"C:\Users\fbj04244.DC00\AppData\Local\Microsoft\Windows\INetCache\Content.Outlook" };
+#endif
+            if (args.Length != 1) {
                 Console.WriteLine("対象ディレクトリを指定してください。");
                 return -1;
             }
 
-            if(!Directory.Exists(args[0])) {
+            if (!Directory.Exists(args[0])) {
                 Console.WriteLine("対象ディレクトリが存在しません。");
                 return -1;
             }
 
-            foreach(var file in Directory.GetFiles(args[0], "*.*", SearchOption.TopDirectoryOnly)) {
-                try { File.Delete(file); } catch { };
+            return DeleteAllSubs(args[0], false);
+        }
+
+        static int DeleteAllSubs(string dir, bool ownFlag = true) {
+            foreach (var subFIles in Directory.GetFiles(dir, "*.*", SearchOption.TopDirectoryOnly)) {
+                try {
+                    var fileInfo = new FileInfo(subFIles);
+                    if ((fileInfo.Attributes & FileAttributes.ReadOnly) == FileAttributes.ReadOnly) {
+                        fileInfo.Attributes &= ~FileAttributes.ReadOnly;
+                    }
+                    if ((fileInfo.Attributes & FileAttributes.System) == FileAttributes.System) {
+                        fileInfo.Attributes &= ~FileAttributes.System;
+                    }
+                    fileInfo.Delete();
+                } catch { }
             }
 
-            foreach(var dir in Directory.GetDirectories(args[0], "*.*", SearchOption.TopDirectoryOnly)) {
-                try { Directory.Delete(dir, true); } catch { };
+            foreach (var subDir in Directory.GetDirectories(dir, "*.*", SearchOption.TopDirectoryOnly)) {
+                DeleteAllSubs(subDir);
+            }
+
+            if (ownFlag) {
+                try {
+                    Directory.Delete(dir);
+                } catch { }
             }
 
             return 0;
